fb53ee33658c8871e72dad69d3af1b692aabee1c
Maroua GHNIMI <maroua.ghnimi@niji.fr>
2024-11-03 23:58:46 +0100

    feat: log position with writer

--- src/main/java/com/example/batchprocessing/Application/LawnMowerWriter.java
+++ src/main/java/com/example/batchprocessing/Application/LawnMowerWriter.java
@@ -1,23 +1,18 @@
 package com.example.batchprocessing.Application;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.Chunk;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.stereotype.Component;
 
 import com.example.batchprocessing.domain.model.Position;
 
-
-
-
-
 public class LawnMowerWriter implements ItemWriter<Position> {
 
-   
+	private static final Logger logger = LoggerFactory.getLogger(LawnMowerWriter.class);
 
-  
-
-    @Override
-    public void write(Chunk<? extends Position> positions) throws Exception {
-        positions.getItems().stream().forEach(Position::toString);
-    }
+	@Override
+	public void write(Chunk<? extends Position> positions) throws Exception {
+		positions.getItems().forEach(position -> logger.info("Position: {}", position.toString()));
+	}
 }
